0ceea4f12aca1d4dc56eba32ab1f3169cee8fc99
Andrew Parry <2393265p@student.gla.ac.uk>
2023-10-24 14:18:29 +0100

    mo convienience

--- idallm/__init__.py
+++ idallm/__init__.py
@@ -1,2 +1,2 @@
 from .parameters import load_generation_config
-from .helper import model_init
+from .helper import model_init, request
--- idallm/helper.py
+++ idallm/helper.py
@@ -1,5 +1,58 @@
 from vllm import LLM
+import json
+from typing import Iterable, List
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+import requests
+
+### REQUEST HELPER FUNCTIONS ###
+
+def clear_line(n: int = 1) -> None:
+    LINE_UP = '\033[1A'
+    LINE_CLEAR = '\x1b[2K'
+    for _ in range(n):
+        print(LINE_UP, end=LINE_CLEAR, flush=True)
+
+def get_streaming_response(response: requests.Response) -> Iterable[List[str]]:
+    for chunk in response.iter_lines(chunk_size=8192,
+                                     decode_unicode=False,
+                                     delimiter=b"\0"):
+        if chunk:
+            data = json.loads(chunk.decode("utf-8"))
+            output = data["text"]
+            yield output
+
+
+def get_response(response: requests.Response) -> List[str]:
+    data = json.loads(response.content)
+    output = data["text"]
+    return output
+
+def post_http_request(prompt: str,
+                      api_url: str,
+                      stream: bool = False,
+                      generation_kwargs : dict = {}) -> requests.Response:
+    pload = {
+        "prompt": prompt, "stream": stream, **generation_kwargs
+    }
+    response = requests.post(api_url, json=pload)
+    return response
+
+### MAIN FUNCTIONS ###
 
 def model_init(model_name_or_path, dtype : str = None, quantization : str = None, **kwargs):
     model = LLM(model=model_name_or_path, dtype=dtype, quantization=quantization, **kwargs)
     return model, model.get_tokenizer()
+
+def request(api_url : str, prompt : str, params : dict = None, config_file : str = None):
+    if not params:
+        params = {}
+    if config_file:
+        params = load(open(config_file, 'r'), Loader=Loader)
+    stream = params.pop('stream', False)
+    response = post_http_request(prompt, api_url, stream, params)
+    
+    return get_streaming_response(response) if stream else get_response(response)
